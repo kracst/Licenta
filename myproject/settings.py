56d9aef6953f035da3465ac4996e35195d345169
kracst <stefbraescu232@gmail.com>
2025-03-09 18:15:55 +0200

    modified the project for deployment

--- myproject/settings.py
+++ myproject/settings.py
@@ -126,5 +126,6 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'\
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
